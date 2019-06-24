226c9329a3a9d52aff883d615506e20584108011
Kong Fanyue <fykong@thoughtworks.com>
2019-06-24 15:02:38 +0800

    Finish practice one

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,24 @@
 package practice01;
 
 public class Person {
+    private String name;
+
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
