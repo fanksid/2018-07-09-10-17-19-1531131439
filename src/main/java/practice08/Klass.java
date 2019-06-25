0263307c9ba217757f5355cf86547a9a219ec9f5
Kong Fanyue <fykong@thoughtworks.com>
2019-06-25 11:27:45 +0800

    Finish practice eight

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,27 @@
 package practice08;
 
 public class Klass {
+    private Integer number;
+
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    Student getLeader() {
+        return leader;
+    }
+
+    void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,39 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Person {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
 }
