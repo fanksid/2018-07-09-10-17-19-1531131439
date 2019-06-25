0c0db6b1f36d23af8e377d89e4da760f5fa76625
Kong Fanyue <fykong@thoughtworks.com>
2019-06-25 13:45:51 +0800

    Finish practice ten

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,45 @@
 package practice10;
 
+import com.google.common.collect.Lists;
+import java.util.List;
+import java.util.Objects;
+
 public class Klass {
+    private Integer number;
+
+    private Student leader;
+
+    private List<Student> members = Lists.newArrayList();
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    Student getLeader() {
+        return leader;
+    }
+
+    void assignLeader(Student student) {
+        if (members.contains(student)) {
+            this.leader = student;
+        }
+
+        System.out.println("It is not one of us.");
+    }
+
+    void appendMember(Student student) {
+        members.add(student);
+    }
+
+    boolean isIn(Student student) {
+        return Objects.equals(this, student.getKlass());
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,56 @@
 package practice10;
 
-public class Teacher {
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
+
+public class Teacher extends Person {
+    private List<Klass> classes;
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Integer id, String name, Integer age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    List<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + findKlassInfo() + ".";
+    }
+
+    private String findKlassInfo() {
+        if (Objects.isNull(classes) || classes.size() == 0) {
+            return "No Class";
+        }
+        return "Class " + getClassSum();
+    }
+
+    private String getClassSum() {
+        List<Integer> classNumbers = classes.stream().map(Klass::getNumber).collect(Collectors.toList());
+        return StringUtils.join(classNumbers, ", ");
+    }
+
+    String introduceWith(Student student) {
+        return super.introduce() + " I am a Teacher. " + findTeachStudentInfo(student) + ".";
+    }
+
+    private String findTeachStudentInfo(Student student) {
+        if (classes.contains(student.getKlass())) {
+            return "I teach " + student.getName();
+        }
+        return "I don't teach " + student.getName();
+    }
+
+    boolean isTeaching(Student student) {
+        return classes.stream().anyMatch(c -> c.isIn(student));
+    }
 }
+
