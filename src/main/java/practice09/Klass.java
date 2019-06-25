19059f6897e1923b85e7850aedfac10583d7162f
Kong Fanyue <fykong@thoughtworks.com>
2019-06-25 11:36:33 +0800

    Finish practice nine

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,40 @@
 package practice09;
 
+import com.google.common.collect.Lists;
+import java.util.List;
+
 public class Klass {
+    private Integer number;
+
+    private Student leader;
+
+    private List<Student> members = Lists.newArrayList();
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    Student getLeader() {
+        return leader;
+    }
+
+    void assignLeader(Student student) {
+        if (members.contains(student)) {
+            this.leader = student;
+        }
+
+        System.out.println("It is not one of us.");
+    }
+
+    void appendMember(Student student) {
+        members.add(student);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,44 @@
 package practice09;
 
-public class Teacher {
+import java.util.Objects;
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + findKlassInfo() + ".";
+    }
+
+    private String findKlassInfo() {
+        if (Objects.isNull(klass)) {
+            return "No Class";
+        }
+        return klass.getDisplayName();
+    }
+
+    String introduceWith(Student student) {
+        return super.introduce() + " I am a Teacher. " + findTeachStudentInfo(student) + ".";
+    }
+
+    private String findTeachStudentInfo(Student student) {
+        if (Objects.equals(student.getKlass(), klass)) {
+            return "I teach " + student.getName();
+        }
+        return "I don't teach " + student.getName();
+    }
 }
+
